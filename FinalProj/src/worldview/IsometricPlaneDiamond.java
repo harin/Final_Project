543b19748ae7fb82e7b7c9c5365d2a627e91a637
harin <harin.sanghirun@gmail.com>
2013-03-27 21:31:48 +0700

    Change old IsometricPlane class to IsometricPlaneDiamond Add new IsometricPlane class (which draws the plane in like the one in the project description)

---
+++ FinalProj/src/worldview/IsometricPlaneDiamond.java
@@ -0,0 +1,51 @@
+package worldview;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class IsometricPlaneDiamond {
+	
+	//draw a isometric square plane with the left corner coordinate x and y
+	//with 'size' box side of 'side' wide tiles.
+	// ______ <- tile
+	//|		|		}
+	//|		|		} side variable refers to the side length of the tile
+	//|_____|		}
+	// size variable refer to the amount of tiles on the board's side.
+	// *note: when the tile is tranform into a isometric projection, it's dimension will change
+	public static void drawBoardTile(Graphics g,int x,int y, int size, int side){
+		int yIncrement = side/2;
+		int xIncrement = side*2;
+		int xDiagIncrement = side;
+		int yDiagIncrement = yIncrement;
+		int run = size;
+		int currentTileX, currentTileY;
+		for(int i=0; i<run;i++){
+			currentTileX = x+(xIncrement*i);
+			currentTileY = y;
+			drawTile(g, currentTileX, currentTileY, side );
+			size--;
+			tilePropUp(g,currentTileX + xDiagIncrement, currentTileY - yDiagIncrement, side, size);
+			tilePropDown(g,currentTileX + xDiagIncrement,currentTileY + yDiagIncrement,side, size);	
+		}
+	}
+	private static void tilePropUp(Graphics g,int x, int y, int side, int size){
+		if(size>0){
+			drawTile(g, x, y, side);
+			tilePropUp(g,x + side,y - side/2, side, --size);
+		}
+	}
+	private static void tilePropDown(Graphics g,int x, int y, int side, int size){
+		if(size>0){
+			drawTile(g, x, y, side);
+			tilePropDown(g,x + side,y + side/2, side, --size);
+		}
+	}
+	
+	public static void drawTile(Graphics g, int x, int y, int side){
+		int [] xCor = {x, x+side, x+side*2, x+side};
+		int [] yCor = {y, y-side/2, y , y+side/2};
+		g.setColor(Color.BLACK);
+		g.drawPolygon(xCor, yCor, 4);
+	}
+}
