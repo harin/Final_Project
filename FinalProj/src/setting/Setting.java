3e3e42f89550cf8a84a32f9b328c9f1b159d77f7
JoeZ Chatchanart <joezcc@gmail.com>
2013-04-05 15:11:04 +0700

    5/4/2556

---
+++ FinalProj/src/setting/Setting.java
@@ -0,0 +1,25 @@
+package setting;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+public class Setting extends JDialog {
+	public Setting(){
+		super(new JFrame(),"Setting and Customization",true);
+		
+		
+		
+		
+		
+		
+		
+		setSize(550,550);
+		
+		
+		
+		
+		
+	}
+	
+
+}
