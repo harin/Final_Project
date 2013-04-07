734ff497b9696f28d4c601367ffc262de6bc9a35
chawin-tiptanagarn <chawin.tip@gmail.com>
2013-04-08 00:05:56 +0700

    position and timestamp clear

--- FinalProj/src/worldview/FetchInformation.java
+++ FinalProj/src/worldview/FetchInformation.java
@@ -29,59 +29,73 @@ public class fetchInformation {
 	  
 	  public fetchInformation(){
 		  try{
-		  URL iceworld = new URL(domain);
-		  URLConnection con = iceworld.openConnection();
-		  BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		  String json = in.readLine();
-		  
-		  parser = new JSONParser();
-		  state = (JSONObject) parser.parse(json);
-		  data = (JSONObject) state.get("data");
-		  
-		  
-		  icetizen = (JSONObject) data.get("icetizen");
-		  
-		  //weather condition
-		  weather = (JSONObject) data.get("weather");
-		  timeWeatherLastChange = (long) weather.get("last_change");
-		  weatherCondition = (String) weather.get("condition");
-		  //end of weather condition
-		  
-		  
-		  for(Object key:keys){
-			  JSONObject userid = (JSONObject) icetizen.get(key);
-			   JSONObject user   = (JSONObject) userid.get("user");
-			   String id = (String) key;
-			   String username =  (String) user.get("username");
-			   int pid = (int) user.get("pid");
-			   String ip = (String) user.get("ip");
-			   
-			   IcetizenLook look =;
-			   
-			   //position and timestamp
-			   JSONObject last_known_destinantion = (JSONObject) userid.get("last_known_destination");
-			   long timestamp = (long) last_known_destinantion.get("timestamp");   
-			   String stringPosition = (String) last_known_destinantion.get("position");
-			   int beginIndex = 1;
-			   int endIndex = stringPosition.indexOf(",");
-			   int beginIndex2 = endIndex+1;
-			   int endIndex2 = stringPosition.indexOf(")");
-			   int x = Integer.parseInt(stringPosition.substring(beginIndex, endIndex));		   
-			   int y = Integer.parseInt(stringPosition.substring(beginIndex2, endIndex2));			   
-			   
-			  int listeningPort=  Integer.parseInt( (String) user.get("port") ) ;
-
-			   
-			   Point position = new Point( x , y);
-			   Point destination = new Point(x,y);
-			   
-			   
-			   
-			  list.add(new NullIcetizen(id,username ,pid,   listeningPort,  look, ip, timestamp,position,destination ));
 			  
-		  }
-		  
-		  
+			  
+			  
+			  URL iceworld = new URL(domain);
+			  URLConnection con = iceworld.openConnection();
+			  BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			  String json = in.readLine();
+			  
+			  parser = new JSONParser();
+			  state = (JSONObject) parser.parse(json);
+			  data = (JSONObject) state.get("data");
+			  
+			  
+			  icetizen = (JSONObject) data.get("icetizen");
+			  
+			  //weather condition
+			  weather = (JSONObject) data.get("weather");
+			  timeWeatherLastChange = (long) weather.get("last_change");
+			  weatherCondition = (String) weather.get("condition");
+			  //end of weather condition
+			  
+			  
+			  for(Object key:keys){
+				   JSONObject userid = (JSONObject) icetizen.get(key);
+				   JSONObject user   = (JSONObject) userid.get("user");
+				   String id = (String) key;
+				   String username =  (String) user.get("username");
+				   int pid = (int) user.get("pid");
+				   String ip = (String) user.get("ip");
+				   
+				   
+				   //position and timestamp
+				   JSONObject last_known_destinantion = (JSONObject) userid.get("last_known_destination");
+				   
+				   try{ //loop2
+					   if(!last_known_destinantion.get("timestamp").equals(null) && !last_known_destinantion.get("position").equals(null)){
+					   
+						   IcetizenLook look =;
+						   long timestamp = (long) last_known_destinantion.get("timestamp");   
+						   String stringPosition = (String) last_known_destinantion.get("position");
+						   int beginIndex = 1;
+						   int endIndex = stringPosition.indexOf(",");
+						   int beginIndex2 = endIndex+1;
+						   int endIndex2 = stringPosition.indexOf(")");
+						   int x = Integer.parseInt(stringPosition.substring(beginIndex, endIndex));		   
+						   int y = Integer.parseInt(stringPosition.substring(beginIndex2, endIndex2));			   
+					   
+						   int listeningPort=  Integer.parseInt( (String) user.get("port") ) ;
+		
+					   
+						   Point position = new Point( x , y);
+						   Point destination = new Point(x,y);
+					   
+					   
+					   
+						   list.add(new NullIcetizen(id,username ,pid,   listeningPort,  look, ip, timestamp,position,destination ));
+					   }
+					   else{
+						   //do nothing
+					   }
+				   }catch(Exception e){
+					   System.out.println("Cannot fetch the information from the server");
+					   System.out.println("Error in try catch2:"+e.toString());
+				   }
+			  }
+			  
+			  
 		}catch(Exception e){
 		//error happen
 			System.out.println("Cannot fetch the information from the server");
