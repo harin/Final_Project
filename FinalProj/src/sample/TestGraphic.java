a099616e2ef3d2d6b942ccdf2980f285f254d87d
chawin-tiptanagarn <chawin.tip@gmail.com>
2013-04-08 21:33:21 +0700

    test fetch graphic

---
+++ FinalProj/src/sample/TestGraphic.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class TestGraphic {
+	public static void main(String[] args){
+		
+		 final String domain =  "http://iceworld.sls-atl.com/api/&cmd=states";
+		 final String domain2 = "http://iceworld.sls-atl.com/api/&cmd=gresources&uid=";
+		 final String test0 = "0";
+		 String tester = domain2+test0;
+		 
+		 
+	}
+	
+	
+}
