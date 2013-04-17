767752220234e91c48c0ef6acec4decfed282d3d
Harin Sanghirun <harin.sanghirun@gmail.com>
2013-04-17 23:17:32 +0700

    Disable buggy code associated with servers

--- FinalProj/src/worldview/SuperSupremeFetchThread.java
+++ FinalProj/src/worldview/SuperSupremeFetchThread.java
@@ -31,7 +31,7 @@ public class SuperSupremeFetchThread extends Thread {
 			System.out.println("set weather to "+weather);
 			mf.updateWeather(weather);
 			
-			//update icetizen
+//			//update icetizen
 //			LinkedList<NullIcetizen> temp = f.getCitizen();
 //			for(NullIcetizen n: temp){
 //				boolean match =false;
@@ -42,76 +42,77 @@ public class SuperSupremeFetchThread extends Thread {
 //				mf.icetizens.add(n);
 //				System.out.println("Added " +n.getUserid() +n.getUsername() +"to icetizens list");
 //			}
-			
-			/**
-			 * ActionType
-			 * 1 = walk
-			 * 2 = talk
-			 * 3 = yell
-			 */
-				for( NullAction n: a){
-					System.out.println("Enter loop");
-					boolean found = false;
-					String uid = n.getUserid();
-					int actionType = n.getActiontype();
-					ActionCommand toUpdate = null;
-					//find if uid already exist
-					for( ActionCommand ac: commands){
-						if(ac.uid.equals(uid)){
-							found = true;
-							toUpdate = ac;
-							break;
-						}
-					}
-					
-					//if doesn't make new one
-					if(!found){
-						toUpdate = new ActionCommand(uid);
-						commands.add(toUpdate);
-					}
-					
-					//update action commands
-					if(toUpdate != null){
-						switch(actionType){
-							case 1:
-								String detail = n.getDetail();
-								String xs = detail.substring(detail.indexOf("(")+1, detail.indexOf(","));
-								int x = Integer.parseInt(xs);
-								String ys = detail.substring(detail.indexOf(",")+1, n.getDetail().indexOf(")"));
-								int y = Integer.parseInt(ys);
-								//System.out.println(uid +" Set walk to " +x+","+y);
-								toUpdate.walkDest = new Point(x,y);
-								break;
-							case 2:
-								toUpdate.talkMsg = n.getDetail();
-								//System.out.println(uid+ "\t Set talk Msg to" + n.getDetail());
-								break;
-							case 3:
-								toUpdate.yellMsg = n.getDetail();
-								//System.out.println(uid+ " Set yell Msg to" + n.getDetail());
-	
-								break;
-						}
-					}
-				}
-				for(ActionCommand ac1: commands){
-					System.out.println(ac1.uid +" talk:"+ac1.talkMsg +
-							"\t yell:"+ ac1.yellMsg +
-							"\t walkDest:" +ac1.walkDest);
-					for(NullIcetizen n:icetizens){
-						if(n.getUserid().equals(ac1.uid)){
-							System.out.println("Setting "+ ac1.uid + n.getUsername()+"walk to" +ac1.walkDest);
-							if(ac1.walkDest!=null)
-								n.setDestination((Point)ac1.walkDest.clone());
-							if(ac1.talkMsg!=null)
-								n.setTalkMsg(ac1.talkMsg);
-							if(ac1.yellMsg!=null)
-								n.setYellMsg(ac1.yellMsg);
-						}
-					}
-				}
-				
-				mf.updateIcetizens(icetizens);
+//			
+//			/**
+//			 * ActionType
+//			 * 1 = walk
+//			 * 2 = talk
+//			 * 3 = yell
+//			 */
+//			// put in action command
+//				for( NullAction n: a){
+//					System.out.println("Enter loop");
+//					boolean found = false;
+//					String uid = n.getUserid();
+//					int actionType = n.getActiontype();
+//					ActionCommand toUpdate = null;
+//					//find if uid already exist
+//					for( ActionCommand ac: commands){
+//						if(ac.uid.equals(uid)){
+//							found = true;
+//							toUpdate = ac;
+//							break;
+//						}
+//					}
+//					
+//					//if doesn't make new one
+//					if(!found){
+//						toUpdate = new ActionCommand(uid);
+//						commands.add(toUpdate);
+//					}
+//					
+//					//update action commands
+//					if(toUpdate != null){
+//						switch(actionType){
+//							case 1:
+//								String detail = n.getDetail();
+//								String xs = detail.substring(detail.indexOf("(")+1, detail.indexOf(","));
+//								int x = Integer.parseInt(xs);
+//								String ys = detail.substring(detail.indexOf(",")+1, n.getDetail().indexOf(")"));
+//								int y = Integer.parseInt(ys);
+//								//System.out.println(uid +" Set walk to " +x+","+y);
+//								toUpdate.walkDest = new Point(x,y);
+//								break;
+//							case 2:
+//								toUpdate.talkMsg = n.getDetail();
+//								//System.out.println(uid+ "\t Set talk Msg to" + n.getDetail());
+//								break;
+//							case 3:
+//								toUpdate.yellMsg = n.getDetail();
+//								//System.out.println(uid+ " Set yell Msg to" + n.getDetail());
+//	
+//								break;
+//						}
+//					}
+//				}
+//				for(ActionCommand ac1: commands){
+//					System.out.println(ac1.uid +" talk:"+ac1.talkMsg +
+//							"\t yell:"+ ac1.yellMsg +
+//							"\t walkDest:" +ac1.walkDest);
+//					for(NullIcetizen n:icetizens){
+//						if(n.getUserid().equals(ac1.uid)){
+//							System.out.println("Setting "+ ac1.uid + n.getUsername()+"walk to" +ac1.walkDest);
+//							if(ac1.walkDest!=null)
+//								n.setDestination((Point)ac1.walkDest.clone());
+//							if(ac1.talkMsg!=null)
+//								n.setTalkMsg(ac1.talkMsg);
+//							if(ac1.yellMsg!=null)
+//								n.setYellMsg(ac1.yellMsg);
+//						}
+//					}
+//				}
+//				
+//				//mf.updateIcetizens(icetizens);
 				System.out.println("-----------------------------------------------------");
 
 				
@@ -121,6 +122,8 @@ public class SuperSupremeFetchThread extends Thread {
 				}catch(Exception e){
 					e.printStackTrace();
 				}
+				
+				
 		}
 	}
 	
