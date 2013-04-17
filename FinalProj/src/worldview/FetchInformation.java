b351e0bc9c1cb923d13a02722549e7323954de85
chawin-tiptanagarn <chawin.tip@gmail.com>
2013-04-17 22:16:40 +0700

    Add method to in fetch information

--- FinalProj/src/worldview/FetchInformation.java
+++ FinalProj/src/worldview/FetchInformation.java
@@ -80,6 +80,195 @@ public class FetchInformation {
 		 this.refreshtime=refreshtime;
 	}
 	
+	public void setFetchNoPic(){
+		setTime();
+		list = new LinkedList <NullIcetizen>();
+		
+		weaponImage = fetchImageFromCloud("http://iceworld.sls-atl.com/graphics/weapon/sword4.png");
+		bodyImage = fetchImageFromCloud("http://iceworld.sls-atl.com/graphics/body/blue.png");
+		shirtImage = fetchImageFromCloud("http://iceworld.sls-atl.com/graphics/shirt/shirt6.png");
+		headImage = fetchImageFromCloud("http://iceworld.sls-atl.com/graphics/head/crown1.png");
+		
+		try {
+
+			URL iceworld = new URL(domain);
+			URLConnection con = iceworld.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String json = in.readLine();
+
+			parser = new JSONParser();
+			state = (JSONObject) parser.parse(json);
+			data = (JSONObject) state.get("data");
+
+			icetizen = (JSONObject) data.get("icetizen");
+
+			// weather condition
+			weather = (JSONObject) data.get("weather");
+			timeWeatherLastChange = (long) weather.get("last_change");
+			weatherCondition = (String) weather.get("condition");
+			// end of weather condition
+			keys = icetizen.keySet();
+			System.out.println(keys);
+			
+			for (Object key : keys) {
+
+				IcetizenLook look = new IcetizenLook();
+				JSONObject userid = (JSONObject) icetizen.get(key);
+				JSONObject user = (JSONObject) userid.get("user");
+				String id = (String) key;
+				String username = (String) user.get("username");
+				int pid = Integer.parseInt(user.get("pid").toString());
+				String ip = (String) user.get("ip");
+				type = (long) user.get("type");
+
+				// position and timestamp
+				JSONObject last_known_destinantion = (JSONObject) userid.get("last_known_destination");
+
+				try { // loop2
+					if ( (""+last_known_destinantion.get("timestamp")).equals("null")&& (""+last_known_destinantion.get("position")).equals("null")) {
+						position = new Point(0, 0);
+						destination = new Point(0,0);
+						timestamp =-1;
+						System.out.println("IS IT WORK");
+						System.out.println("ID:"+key);
+			//			System.out.println("Case of null position:"+ (""+last_known_destinantion.get("position")).equals("null") ) ;
+		//				System.out.println("Case of null timestamp:"+(""+last_known_destinantion.get("timestamp")).equals("null") )	;
+					}
+					else{
+						System.out.println("======================");
+						System.out.println("ID with no null:"+key);
+						if( (""+ last_known_destinantion.get("timestamp")).equals("null") ){
+					
+							timestamp =-1;
+							
+							System.out.println("ID88?");
+							System.out.println("ID:"+key);
+						}
+						else{
+							timestamp =  Long.parseLong(""+ last_known_destinantion.get("timestamp") );
+						}
+						String stringPosition = (String) last_known_destinantion.get("position");
+						System.out.println("Timestamp is:"+timestamp);
+						int beginIndex = 1;
+						int endIndex = stringPosition.indexOf(",");
+						int beginIndex2 = endIndex + 1;
+						int endIndex2 = stringPosition.indexOf(")");
+						
+						int x = Integer.parseInt(stringPosition.substring(beginIndex, endIndex));
+							// walk out of bound
+							if(x<0){
+								x=0;
+							}
+						int y = Integer.parseInt(stringPosition.substring(beginIndex2, endIndex2));
+							// walk out of bound
+							if(y<0){
+								y=0;
+							}
+						listeningPort = Integer.parseInt(user.get("port").toString());
+
+						position = new Point(x, y);
+						destination = new Point(x, y);
+					}
+						
+						
+						// Doing icetizenLook
+						// object key ---> userid
+						// domain2 =
+						// "http://iceworld.sls-atl.com/api/&cmd=gresources&uid=";
+						// domain3 =
+						// "http://iceworld.sls-atl.com/api/&cmd=gurl&gid=";
+						// domain4 = "http://iceworld.sls-atl.com/";
+						//
+						try {//3
+							String userLook = domain2 + key.toString();
+							URL userLookurl = new URL(userLook);
+							URLConnection lookCon = userLookurl.openConnection();
+							BufferedReader lookBuff = new BufferedReader(new InputStreamReader(lookCon.getInputStream()));
+							String jcloth = lookBuff.readLine();
+							System.out.println("GETTING SOMETHING###"+jcloth);
+
+							JSONParser dream = new JSONParser();
+							JSONObject gresource = (JSONObject) dream.parse(jcloth);
+							Set keyInResource = gresource.keySet();
+
+							JSONArray cloth = (JSONArray) gresource.get("data"); // cloth = data is a array
+																				
+
+							String clothString = cloth.toJSONString(); // cloth in string
+																
+							clothString = clothString.substring(1,clothString.length() - 1);// [ ] to cut out the bracket
+																
+							JSONParser clothparser = new JSONParser();
+							JSONObject clothSon = (JSONObject) clothparser.parse(clothString);
+							try{
+								if(timestamp!=-1 && ! (""+clothSon.get("W")).equals("null") ){
+								//if(timestamp!=-1 && !id.equalsIgnoreCase("66") && !id.equalsIgnoreCase("77") && !clothSon.get("W").toString().equals(null) ){
+	
+									System.out.println("ID:"+key+" =====case1");
+									weapon = clothSon.get("W").toString();
+									body = clothSon.get("B").toString();
+									shirt = clothSon.get("S").toString();
+									head = clothSon.get("H").toString();
+								}else{
+									System.out.println("ID:"+key+" =====case2");
+									System.out.println("timestamp is:"+timestamp);
+									weapon = "W075";
+									body = "B001";
+									shirt ="S019";
+									head = "H110";
+								
+							}
+							}catch(Exception e){
+								System.out.println("Errror in loop4:"+e);
+								e.printStackTrace();
+							}
+//							String weaponURL =domain4+getURL(weapon);
+//							String bodyURL =  domain4 + getURL(body);
+//							String shirtURL = domain4 + getURL(shirt);
+//							String headURL =  domain4 + getURL(head);
+							
+					//		System.out.println("weaponURL is"+weaponURL);	
+							
+//							weaponImage = fetchImageFromCloud(weaponURL);
+//							bodyImage = fetchImageFromCloud(bodyURL);
+//							shirtImage = fetchImageFromCloud(shirtURL);
+//							headImage = fetchImageFromCloud(headURL);
+	
+							// set the look!?!?!!??!??
+								
+							look.gidB = weapon;
+							look.gidS = shirt;
+							look.gidH = head;
+							look.gidW = weapon;
+							
+						list.add(new NullIcetizen(id, username, pid,listeningPort, look, ip, type, timestamp,position, destination, headImage, bodyImage, shirtImage,weaponImage));
+
+						} catch (Exception e) {
+							System.out.println("error during get the look at loop3: " + e);
+							e.printStackTrace();		
+							}
+						
+
+						
+				} catch (Exception e) {
+					System.out.println("Cannot fetch the information from the server");
+					System.out.println("Error in try catch2:" + e.toString());
+					e.printStackTrace();		
+
+				}
+			}
+
+		} catch (Exception e) {
+			// error happen
+			System.out.println("Cannot fetch the information from the server loop1");
+			System.out.println("Error:" + e.toString());
+			e.printStackTrace();		
+
+		}
+		System.out.println("Fetching OK");
+		setAction(currentTime);
+	}
+	
 	public void setFetchState() {
 		setTime();
 		list = new LinkedList <NullIcetizen>();
